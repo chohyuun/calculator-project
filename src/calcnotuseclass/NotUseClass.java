128b5a5d26094d1e25dcf34629a5202f645a37f0
조현지 <qndjddl001@gmail.com>
2024-10-15 15:57:31 +0900

    fix: 입력 타입 변경, 무한 입력 기능 추가
    실수 연산을 위한 입력 타입 변환
    exit 입력 시 반복 종료 하도록 변경 및 타입 변환 기능 추가

--- src/calcnotuseclass/NotUseClass.java
+++ src/calcnotuseclass/NotUseClass.java
@@ -5,50 +5,60 @@ import java.util.Scanner;
 // 이후 클래스 calc 사용을 위해 따로 넣음
 public class NotUseClass {
     public static void main(String[] args) {
-        // todo : 나누기 연산 결과 실수형으로 변환
         Scanner sc = new Scanner(System.in);
 
-        int result = 0;
-        int operand1 = 0, operand2 = 0;
+        double result = 0;
+        double operand1 = 0;
+        double operand2 = 0;
 
-        // 숫자 입력 후 숫자가 맞는지 확인
-        try {
-            System.out.print("피연산자를 입력해 주세요(숫자 2개) : ");
-            operand1 = sc.nextInt();
-            operand2 = sc.nextInt();
+        while (true) {
+            try {
+                System.out.print("피연산자를 입력해 주세요(숫자 2개/ 종료: exit) : ");
+                String input1 = sc.next();
+                if(input1.equals("exit")){
+                    break;
+                }
+                String input2 = sc.next();
+                if(input2.equals("exit")){
+                    break;
+                }
 
-            System.out.print("연산자를 입력해 주세요.(+, -, *, /, %) : ");
-            String operation = sc.next();
+                operand1 = Double.parseDouble(input1);
+                operand2 = Double.parseDouble(input2);
 
-            switch (operation) {
-                case "+":
-                    result = operand1 + operand2;
-                    break;
-                case "-":
-                    result = operand1 - operand2;
-                    break;
-                case "*":
-                    result = operand1 * operand2;
-                    break;
-                case "/":
-                    result = operand1 / operand2;
-                    break;
-                case "%":
-                    result = operand1 % operand2;
-                    break;
-                default:
-                    // 틀린 연산자 입력의 경우 예외 처리
-                    System.out.println("사용 가능한 연산자를 입력해 주세요.");
-                    break;
+                System.out.print("연산자를 입력해 주세요.(+, -, *, /, %) : ");
+                String operation = sc.next();
+
+                switch (operation) {
+                    case "+":
+                        result = operand1 + operand2;
+                        break;
+                    case "-":
+                        result = operand1 - operand2;
+                        break;
+                    case "*":
+                        result = operand1 * operand2;
+                        break;
+                    case "/":
+                        result = operand1 / operand2;
+                        break;
+                    case "%":
+                        result = operand1 % operand2;
+                        break;
+                    default:
+                        // 틀린 연산자 입력의 경우 예외 처리
+                        System.out.println("사용 가능한 연산자를 입력해 주세요.");
+                        break;
+                }
+            } catch (NumberFormatException e) {
+                // 입력받은 값이 숫자가 아닌 경우 예외 처리
+                System.out.println("숫자를 입력해 주세요.");
+            } catch (ArithmeticException e) {
+                // 나누기 연산 중 분모가 0일 경우 예외 처리
+                System.out.println(e.getMessage());
             }
-        } catch (NumberFormatException e) {
-            // 입력받은 값이 숫자가 아닌 경우 예외 처리
-            System.out.println("숫자를 입력해 주세요.");
-        } catch (ArithmeticException e) {
-            // 나누기 연산 중 분모가 0일 경우 예외 처리
-            System.out.println(e.getMessage());
-        }
 
-        System.out.println("결과 : " + result);
+            System.out.println("결과 : " + result);
+        }
     }
 }
